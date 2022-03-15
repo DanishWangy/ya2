05ec1d30d50a1a33e90138bf77cf7f8641ff08fc
DanishWangy <danishkunwangy@gmail.com>
2022-03-15 21:28:18 +0700

    ya2

---
+++ config.js
@@ -0,0 +1,51 @@
+let fs = require('fs')
+global.owner = ['6281313144291', '6285895774939','18133081474', '62881036210922', '62858957749391'] // Letakan nomor kamu disini
+global.APIs = { // API Prefix
+  // nama: 'https://website'
+  jonaz: 'https://jonaz-api-v2.herokuapp.com',
+  neoxr: 'https://api.neoxr.eu.org/',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zekais: 'http://zekais-api.herokuapp.com',
+  zeks: 'https://api.zeks.me',
+  vhtear: 'https://api.vhtear.com',
+  lolhum: 'https://api.lolhuman.xyz',
+  bx: 'https://bx-hunter.herokuapp.com',
+
+}
+global.APIKeys = { // APIKey nya disini
+  // 'https://website': 'apikey'
+  'https://api.neoxr.eu.org/': 'yntkts',
+  'https://pencarikode.xyz': 'APIKEY',
+  'https://api.xteam.xyz': 'HIRO',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.me': 'apivinz',
+  'https://api.lolhuman.xyz': '929b48b9aa5a91abed8526cd',
+  'https://api.vhtear.com': 'sayahafiz',
+  'http://zekais-api.herokuapp.com': 'grqgD6pU',
+  'https://bx-hunter.herokuapp.com': 'Ikyy69',
+}
+
+// Sticker WM
+global.packname = 'DaniBotz'
+global.author = '0813-1314-4291'
+
+global.wait = '「 ⏱️ 」Harap tunggu...'
+global.eror = '「❗」Server ERROR!'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
+global.watermark = '© DaniBotz'
+global.thumbfoto = 'https://i.ibb.co/9rMGPRY/2249d5c5fa9785d2682aa.jpg' 
+global.image = 'https://i.ibb.co/9rMGPRY/2249d5c5fa9785d2682aa.jpg'
+
+global.multiplier = 59 // Semakin tinggi, semakin sulit naik level
+
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
---
+++ plugins/donasi.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
+┌〔 Donasi • Emoney 〕
+├ GoPay: 081313144291
+├ Dana: 081313144291
+├ Pulsa (Tsel): 081313144291
+└────
+`.trim(), watermark, 'Menu', '.menu', m)
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
